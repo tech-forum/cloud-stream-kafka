2368e5f3e6764fec72f89415bc26486bc1d92f88
tech_forum <38194697+tech-forum@users.noreply.github.com>
2018-08-20 23:09:05 -0400

    Removed producer code

--- src/main/java/com/techforum/pubsub/kafka/controller/MessageController.java
+++ src/main/java/com/techforum/pubsub/kafka/controller/MessageController.java
@@ -1,37 +1,37 @@
-package com.techforum.pubsub.kafka.controller;
-
-import com.esotericsoftware.minlog.Log;
-import com.techforum.pubsub.kafka.listener.MessageSubscriber;
-import com.techforum.pubsub.kafka.service.MessageService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class MessageController {
-
-    Logger LOG = LoggerFactory.getLogger(MessageSubscriber.class);
-
-    private final MessageService transactionService;
-
-    @Autowired
-    public MessageController(MessageService transactionService) {
-        this.transactionService = transactionService;
-    }
-
-    @GetMapping("/message")
-    public String message(@RequestParam("content") String message){
-
-        Log.info("Starts message : {} ", message);
-        try {
-            transactionService.sendTextMessage(message);
-            return "MESSAGE SENT SUCCESSFULLY";
-        }catch(Exception ex){
-            LOG.info("Exception occurred:",ex);
-            return "MESSAGE FAILED TO SENT";
-        }
-    }
-}
+//package com.techforum.pubsub.kafka.controller;
+//
+//import com.esotericsoftware.minlog.Log;
+//import com.techforum.pubsub.kafka.listener.MessageSubscriber;
+//import com.techforum.pubsub.kafka.service.MessageService;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class MessageController {
+//
+//    Logger LOG = LoggerFactory.getLogger(MessageSubscriber.class);
+//
+//    private final MessageService transactionService;
+//
+//    @Autowired
+//    public MessageController(MessageService transactionService) {
+//        this.transactionService = transactionService;
+//    }
+//
+//    @GetMapping("/message")
+//    public String message(@RequestParam("content") String message){
+//
+//        Log.info("Starts message : {} ", message);
+//        try {
+//            transactionService.sendTextMessage(message);
+//            return "MESSAGE SENT SUCCESSFULLY";
+//        }catch(Exception ex){
+//            LOG.info("Exception occurred:",ex);
+//            return "MESSAGE FAILED TO SENT";
+//        }
+//    }
+//}
--- src/main/java/com/techforum/pubsub/kafka/listener/MessageSubscriber.java
+++ src/main/java/com/techforum/pubsub/kafka/listener/MessageSubscriber.java
@@ -4,7 +4,7 @@ import com.techforum.pubsub.kafka.config.TechforumStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.integration.kafka.listener.Acknowledgment;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
@@ -16,11 +16,17 @@ public class MessageSubscriber {
     @StreamListener(TechforumStream.INPUT)
     public void onMessage(Message message){
         byte[] payload = (byte[]) message.getPayload();
-        LOG.info("payload : {}", new String(payload));
-        Acknowledgment acknowledgment = message.getHeaders().get(KafkaHeaders.ACKNOWLEDGMENT, Acknowledgment.class);
-        if (acknowledgment != null) {
-            LOG.info("Acknowledgment provided");
+        String strPayload =  new String(payload);
+        LOG.info("payload : {}", strPayload);
+        if(strPayload.contains("5")) {
+
+        } else {
+            Acknowledgment acknowledgment = message.getHeaders().get(KafkaHeaders.ACKNOWLEDGMENT, Acknowledgment.class);
             acknowledgment.acknowledge();
+            if (acknowledgment != null) {
+                LOG.info("Acknowledgment provided");
+                acknowledgment.acknowledge();
+            }
         }
     }
 }
